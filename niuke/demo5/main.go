b5296402e0d1cde47d9fdc850947486488f8ebc2
yuwe1 <yuweijiubayiqi@163.com>
2020-09-06 14:45:42 +0800

    修改快速排序

--- niuke/demo5/main.go
+++ niuke/demo5/main.go
@@ -2,23 +2,23 @@ package main
 
 import "fmt"
 
-func helper(a []int,left,right int)int{
-	l,r :=left,right
-	mid :=(l+r) / 2
-	temp :=a[mid]
+func helper(a []int, left, right int) int {
+	l, r := left, right
+	mid := (l + r) / 2
+	temp := a[mid]
 	for l <= r {
-		for r >= mid && a[r] >= temp{
+		for r >= mid && a[r] >= temp {
 			r--
 		}
-		if r >= mid{
-			a[r],a[mid] = a[mid],a[r]
+		if r >= mid {
+			a[r], a[mid] = a[mid], a[r]
 			mid = r
 		}
-		for l <= mid && a[l] <= temp{
+		for l <= mid && a[l] <= temp {
 			l++
 		}
-		if l <= mid{
-			a[l],a[mid] = a[mid],a[l]
+		if l <= mid {
+			a[l], a[mid] = a[mid], a[l]
 			mid = l
 		}
 	}
@@ -26,18 +26,33 @@ func helper(a []int,left,right int)int{
 	return mid
 }
 
-func QuickSort(list []int,low,high int)  {
-	if high > low{
+func QuickSort(list []int, low, high int) {
+	if high > low {
 		//位置划分
-		pivot := helper(list,low,high)
+		pivot := helperv1(list, low, high)
 		//左边部分排序
-		QuickSort(list,low,pivot-1)
+		QuickSort(list, low, pivot-1)
 		//右边排序
-		QuickSort(list,pivot+1,high)
+		QuickSort(list, pivot+1, high)
 	}
 }
+func helperv1(data []int, left, right int) int {
+	temp := data[left] //基准值
+	mid := left
+
+	for i := left + 1; i <= right; i++ {
+		if temp > data[i] {
+			data[i], data[mid+1] = data[mid+1], data[i]
+			mid++
+		}
+	}
+	data[left], data[mid] = data[mid], data[left]
+	return mid
+
+}
+
 func main() {
-	a :=[]int{2,3,1,5,3,5,8,9,0,10,4,2}
-	QuickSort(a,0,len(a)-1)
+	a := []int{2, 3, 1, 5, 3, 5, 8, 9, 0, 10, 4, 2}
+	QuickSort(a, 0, len(a)-1)
 	fmt.Println(a)
 }
